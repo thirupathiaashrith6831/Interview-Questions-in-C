9ccb8aceb17c7fa9b2b21740fce33333eea15514
thirupathiaashrith6831 <thirupathiaashrith6831@gmail.com>
2026-02-02 16:02:15 +0530

    Add files via upload

---
+++ Red_lottery.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+    int a,b,c;
+    printf("Enter the [a,b,c] between 0,1,2 values:\n");
+    scanf("%d,%d,%d",&a,&b,&c);
+    if (a==2 && b==2 && c==2)
+    {
+        printf("10");
+    }
+    else if(a==b && b==c && c==a)
+    {
+        printf("5");
+    }
+    else if(b!=a && c!=a)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
+    }
+}
